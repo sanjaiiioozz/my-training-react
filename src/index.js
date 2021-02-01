357548ac353da73d14099704bff06f044c29cb8d
Grid <sanjaikornsutatipkul@gmail.com>
2021-02-01 13:49:19 +0700

    IntroToComponents

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+// react convention https://github.com/airbnb/javascript/tree/master/react#naming
+import Heading1 from "./components/Heading";
+import UnorderedList from "./components/UnorderedList";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <Heading1 />
+    <UnorderedList />
+    <UnorderedList />
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
