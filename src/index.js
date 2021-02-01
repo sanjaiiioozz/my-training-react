9a98157b7000d5114716a5a52ad4fe4253400157
Grid <sanjaikornsutatipkul@gmail.com>
2021-02-01 13:22:44 +0700

    Html Global attributes and JS in html attr

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const img = "https://picsum.photos/200";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    {/* html global attrbutes for js need to be camel case */}
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <div>
+      <img className="food-img" alt="bacon" src={img + "?grayscale"} />
+      <img className="food-img" alt="robot" src={img+ "?grayscale"} />
+      <img className="food-img" alt="robot" src={img+ "?grayscale"} />
+    </div>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
